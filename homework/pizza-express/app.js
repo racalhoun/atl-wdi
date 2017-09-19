f90555e47a84b2c6be3ca41422209e368674c0a0
racalhoun <racalhoun489@gmail.com>
2017-09-19 18:28:20 -0400

    working greeting

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -3,8 +3,39 @@ var express = require('express');
 //save an express module as 'app'
 var app = express();
 // assigning 3000 as our port
-var port = 3000;
+const hbs = require('hbs');
 
+
+app.set("view engine", "hbs");
+
+app.get('/greeting', function(req, res) {
+  const hello = "Welcome to Pizza Express!!"
+  res.render('greeting', {
+      hello: hello
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var port = 3000;
 // tells the server to listen for requests on port 3000
 app.listen(port, function(){
   console.log("==========================")
