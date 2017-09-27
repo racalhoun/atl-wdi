61b6d18e44c6582176db1ad5b4979e0375885af4
racalhoun <racalhoun489@gmail.com>
2017-09-26 23:02:30 -0400

    routes set up

--- homework/mongo/donut-shop/donut_mongoose/controllers/donuts.js
+++ homework/mongo/donut-shop/donut_mongoose/controllers/donuts.js
@@ -10,21 +10,39 @@ const DonutModel = Schema.DonutsModel
 // INDEX
 //======================
 // Create a GET index route "/" that sends all donuts to index.hbs
-
-
+router.get('/', (req, res) =>{
+DonutModel.find({})
+    .then((donuts)=>{
+    response.render('donuts/index', {
+       donuts: donuts 
+    })
+})
+.catch((error) =>{
+    console.log(error)
+});
 
 //======================
 // NEW
 //======================
 // Create a GET new route "/new" that renders the new.hbs form
-
+router.get('/new', (req, res)=>{
+    response.render('donuts/new')
+})
 
 
 //======================
 // SHOW
 //======================
 // Create a GET show route "/:id" that renders the donut's show page
-
+router.get('/:id', (req, res)=>{
+    const donutId = request.params.donutId
+    DonutModel.findById(donutId)
+    .then((donut)=>{
+        response.render('donuts/show', {
+            donut: donut
+        })
+    })
+  })
 
 
 
@@ -33,7 +51,16 @@ const DonutModel = Schema.DonutsModel
 //======================
 // Create a POST index route "/" that creates a new donut
 // and upon success redirects back to the index page "/"
-
+router.post('/', (req, res)=>{
+    const newDonut = request.body
+    DonutModel.create(newDonut)
+    .then(()=>{
+        response.redirect('/donuts')
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+})
 
 
 //======================
@@ -41,7 +68,18 @@ const DonutModel = Schema.DonutsModel
 //======================
 // Create a GET edit route "/:id/edit" that renders the edit.hbs page and
 // sends that donut's data to it
-
+router.get('/:id/edit', (req, res)=>{
+    const donutId = req.params.id
+    DonutModel.findById(donutId)
+    .then((donut)=>{
+        response.render('donuts/edit',{
+            donut: donut
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    })
+})
 
 
 //======================
@@ -49,7 +87,17 @@ const DonutModel = Schema.DonutsModel
 //======================
 // Create a PUT update route "/:id" that updates the donut and
 // redirects back to the SHOW PAGE (not index)
-
+router.put('/:id', (req, res)=>{
+    const donutIdToUpdate = req.params.id
+    const updatedDonut = req.body
+    DonutModel.findByIdAndUpdate({id: donutIdToUpdate}, updatedDonut, {new: true})
+    .then(()=>{
+    response.redirect(`/donuts/${donutIdToUpdate}`)
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+})
 
 
 //======================
@@ -57,9 +105,17 @@ const DonutModel = Schema.DonutsModel
 //======================
 // Create a DELETE delete route "/:id" that deletes the donut and
 // redirects back to index page "/"
-
-
-
+router.delete('/:id/delete', (req, res)=>{
+    const donutId = req.params.id
+    DonutModel.findByIdAndRemove(donutId)
+    .then((donut)=>{
+        response.redirect('/donuts')
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+})
+});
 //======================
 // EXPORTS
 //======================
